f5b6873b9d019f9857611ac9ba93e766d833d357
Robyn Navarro <rnavarro182@gmail.com>
2018-07-08 17:15:39 -0700

    router and requestHandlers added

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const http = require('http');
+const url = require('url');
 
-function start() {
+function start(route, handle) {
     http.createServer((request, response) => {
-        console.log('Request received');
+        const pathname = url.parse(request.url).pathname;
+        console.log(`Request for ${pathname} received`);
+
+        
         response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write('Hello World');
+        const content = route(handle, pathname);
+        response.write(content);
         response.end();
     }).listen(8888);
     console.log('Server has started');
