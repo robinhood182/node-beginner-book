0cb57b78e4700631db291d76957e6a7f14e88028
Robyn Navarro <rnavarro182@gmail.com>
2018-07-08 20:06:45 -0700

    submitted text now displays in upload path

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const url = require('url');
 
 function start(route, handle) {
     http.createServer((request, response) => {
+        let postData = '';
         const pathname = url.parse(request.url).pathname;
         console.log(`Request for ${pathname} received`);
+
+        request.setEncoding('utf8');
+
+        request.addListener('data', postDataChunk => {
+            postData += postDataChunk;
+            console.log(`Received POST data chunk '${postDataChunk}'.`);
+        });
+
+        request.addListener('end', () => {
+            route(handle, pathname, response, postData);
+        });
   
-        route(handle, pathname, response);
     }).listen(8888);
-    
+
     console.log('Server has started');
 }
 
